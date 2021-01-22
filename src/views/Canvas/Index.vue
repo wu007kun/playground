e07cb9c7e0c4834b5414f452e2faed5d330ea504
wukun <wukun@cleartv.cn>
2021-01-22 18:05:06 +0800

    commit

--- src/views/Canvas/Index.vue
+++ src/views/Canvas/Index.vue
@@ -1,11 +1,29 @@
 <template>
   <div class="canvas-page">
-    <div class="ctrls">
-      <button @click="addPolygon">新增polygon</button>
-      <button @click="addRect">新增rect</button>
+    <div class="canvas-section">
+      <div class="ctrls">
+        <div class="btn" @click="emitUpload">
+          替换图片
+          <input style="display: none" type="file" ref="upload" @change="setImage">
+        </div>
+        <div class="btn" @click="addPolygon">画多边形</div>
+        <div class="btn" @click="addRect">画矩形</div>
+        <p class="tip">{{ tip }}</p>
+      </div>
+      <div class="wrap">
+        <canvas id="canvas-elem" width="500" height="500"></canvas>
+      </div>
     </div>
-    <div class="wrap">
-      <canvas id="canvas-elem" width="500" height="500"></canvas>
+    <div class="data-section">
+      <div class="data-item"
+        v-for="(item, index) in pointsData"
+        :key="index"
+        :class="{
+          'active': item.active
+        }">
+        <p>类型：{{ nameDic[item.name] }}</p>
+        <p>数据：{{ JSON.stringify(item.data) }}</p>
+      </div>
     </div>
   </div>
 </template>
@@ -13,7 +31,6 @@
 import CanLib from './CanLib/index'
 let sandbox = null // 画布
 let bgImage = null // 背景图片类
-const entities = [] // 主要覆盖物
 const ctrlItems = [] // 被选中的覆盖物的顶点圆
 const tempItems = [] // 绘制途中产生的临时点线
 const rectVertexArr = [['xmin', 'ymin'], ['xmax', 'ymax']] // 矩形的控制点名称
@@ -21,18 +38,69 @@ const rectVertexArr = [['xmin', 'ymin'], ['xmax', 'ymax']] // 矩形的控制点
 export default {
   data () {
     return {
+      nameDic: {
+        polygon: '多边形',
+        rect: '矩形'
+      },
       size: 500,
+      entities: [], // 主要覆盖物
       newPoints: [], // 绘制过程的拾取点
       newEntity: null, // 当前绘制的覆盖物
-      activeEntity: null // 当前激活编辑的覆盖物
+      activeEntity: null, // 当前激活编辑的覆盖物
+      status: 0,
+      keyHandler: {}
+    }
+  },
+  computed: {
+    pointsData () {
+      return this.entities.map(et => ({
+        name: et.entityType,
+        active: this.activeEntity === et,
+        data: et.points
+      }))
+    },
+    tip () {
+      if (this.status === 0) {
+        return '状态：空闲'
+      } else if (this.status === 1) {
+        return '画多边形：左键绘制多边形，右键回退，回车确认'
+      } else if (this.status === 2) {
+        return '画矩形：左键选择矩形的两个对角顶点'
+      } else if (this.status === 3) {
+        return '编辑：拖动顶点进行编辑，右键返回，Delete删除'
+      } else {
+        return ''
+      }
     }
   },
   mounted () {
+    document.addEventListener('keyup', e => {
+      this.handleKey(e.key)
+    })
     this.initCanvas()
     const img = require('./bg.jpg')
     this.initBgImage(img)
+    this.initExistEntities()
   },
   methods: {
+    handleKey (key) {
+      if (this.keyHandler[key]) {
+        for (const callback of this.keyHandler[key].values()) {
+          callback()
+        }
+      }
+    },
+    addKeyHandler (key, callback) {
+      if (!this.keyHandler[key]) {
+        this.keyHandler[key] = new Set()
+      }
+      this.keyHandler[key].add(callback)
+    },
+    removeKeyHandler (key, callback) {
+      if (this.keyHandler[key]) {
+        this.keyHandler[key].delete(callback)
+      }
+    },
     // 初始化
     initCanvas () {
       const c = document.getElementById('canvas-elem')
@@ -70,9 +138,36 @@ export default {
         image.src = src
       })
     },
+    emitUpload () {
+      if (this.status !== 0) return
+      this.$refs.upload.click()
+    },
+    setImage () {
+      const file = this.$refs.upload.files[0]
+      const reader = new FileReader()
+      reader.readAsDataURL(file)
+      reader.onload = (e) => {
+        const imgBase64Data = e.target.result
+        console.log(imgBase64Data)
+        const image = document.createElement('img')
+        image.onload = () => {
+          let w = this.size
+          let h = this.size
+          if (image.width >= image.height) {
+            h = this.size / image.width * image.height
+          } else {
+            w = this.size / image.height * image.width
+          }
+          bgImage.width = w
+          bgImage.height = h
+          bgImage.image = image
+        }
+        image.src = imgBase64Data
+      }
+    },
     initExistEntities () {
-      const et = new CanLib.Polygon({
-        sandbox,
+      const et = this.drawPolygon({
+        color: 'rgba(100, 155, 255, .7)',
         points: [
           [50, 50],
           [100, 40],
@@ -80,22 +175,22 @@ export default {
           [130, 150],
           [50, 200],
           [30, 100]
-        ],
-        color: '#ddccdd'
+        ]
       })
-      sandbox.add(et)
+      this.entities.push(et)
       et.on('click', e => {
         this.setActiveEntity(et)
       })
-      entities.push(et)
     },
     // 新建多边形部分
     // 开始新建多边形
     addPolygon () {
+      if (this.status !== 0) return
+      this.status = 1
       this.newPoints = []
       sandbox.on('click', this.pickPolygon)
       sandbox.on('contextmenu', this.backstep)
-      document.addEventListener('keyup', this.completePolygon)
+      this.addKeyHandler('Enter', this.completePolygon)
     },
     // 左键拾取顶点
     pickPolygon (e) {
@@ -111,7 +206,7 @@ export default {
       tempItems.push(vertex)
       if (this.newPoints.length === 3) {
         this.newEntity = this.drawPolygon({
-          color: 'rgba(200, 100, 110, .3)',
+          color: 'rgba(100, 155, 255, .7)',
           points: this.newPoints
         })
       } else if (this.newPoints.length > 3 && this.newEntity) {
@@ -146,26 +241,28 @@ export default {
     },
     // 回车键 完成新建
     completePolygon (e) {
-      if (e.key === 'Enter') {
-        sandbox.off('click', this.pickPolygon)
-        sandbox.off('contextmenu', this.backstep)
-        this.newPoints = []
-        tempItems.forEach(v => {
-          sandbox.remove(v)
-        })
-        tempItems.length = 0
-        const et = this.newEntity
-        et.on('click', e => {
-          this.setActiveEntity(et)
-        })
-        this.newEntity = null
-        document.removeEventListener('keyup', this.completePolygon)
-      }
+      sandbox.off('click', this.pickPolygon)
+      sandbox.off('contextmenu', this.backstep)
+      this.newPoints = []
+      tempItems.forEach(v => {
+        sandbox.remove(v)
+      })
+      tempItems.length = 0
+      const et = this.newEntity
+      this.entities.push(et)
+      et.on('click', e => {
+        this.setActiveEntity(et)
+      })
+      this.newEntity = null
+      this.removeKeyHandler('Enter', this.completePolygon)
+      this.status = 0
     },
 
     // 新建矩形部分
     // 开始新建矩形
     addRect () {
+      if (this.status !== 0) return
+      this.status = 2
       this.newPoints = []
       sandbox.on('click', this.pickRect)
       sandbox.on('contextmenu', this.backstepRect)
@@ -184,7 +281,7 @@ export default {
           const ymin = y1 < y2 ? y1 : y2
           const ymax = y1 < y2 ? y2 : y1
           const entity = this.drawRect({
-            color: 'rgba(200, 100, 110, .3)',
+            color: 'rgba(100, 205, 110, .7)',
             xmin,
             xmax,
             ymin,
@@ -200,6 +297,7 @@ export default {
           tempItems.length = 0
           sandbox.off('click', this.pickRect)
           sandbox.off('contextmenu', this.backstepRect)
+          this.status = 0
         }
       } else {
         this.newPoints.push(e)
@@ -225,7 +323,7 @@ export default {
         color: params.color
       })
       sandbox.add(rect)
-      entities.push(rect)
+      this.entities.push(rect)
       return rect
     },
     // 右键回退一步
@@ -239,8 +337,12 @@ export default {
     // 覆盖物编辑部分
     // 点击一个覆盖物，激活编辑状态
     setActiveEntity (et) {
+      if (this.status !== 0) return
+      this.status = 3
       this.activeEntity = et
       this.clearCtrlItems()
+      sandbox.on('contextmenu', this.exitEditing)
+      this.addKeyHandler('Delete', this.handleDelete)
       if (et.entityType === 'polygon') {
         et.points.forEach((pos, index) => {
           const circle = new CanLib.Circle({
@@ -278,6 +380,28 @@ export default {
           })
         })
       }
+    },
+    // 退出编辑
+    exitEditing () {
+      this.status = 0
+      this.activeEntity = null
+      this.clearCtrlItems()
+      sandbox.off('contextmenu', this.exitEditing)
+    },
+    // 删除一个覆盖物
+    handleDelete () {
+      console.log(this.status)
+      if (this.status !== 3) return
+      console.log('删除前', this.entities)
+      const i = this.entities.indexOf(this.activeEntity)
+      console.log('索引', i)
+      this.entities.splice(i, 1)
+      this.activeEntity.destruct()
+      this.activeEntity = null
+      console.log('删除后', this.entities)
+      this.clearCtrlItems()
+      this.status = 0
+      this.removeKeyHandler('Delete', this.handleDelete)
     },
     // 多边形，拖拽顶点
     moveCircle (e) {
@@ -319,8 +443,25 @@ export default {
 </script>
 <style lang="less">
 .canvas-page {
+  font-size: 14px;
+  padding: 30px;
+  display: flex; align-items: flex-start;
+  .ctrls {
+    margin-bottom: 10px;
+    display: flex;
+    .btn {
+      background: #409eff; color: #fff;
+      margin-right: 10px;
+      padding: 3px 6px; font-size: 12px;
+      border-radius: 3px;
+      cursor: pointer; user-select: none;
+    }
+    .tip {
+      color: #3333ff;
+      font-weight: bold;
+    }
+  }
   .wrap {
-    margin: 0 auto;
     display: block;
     width: 500px; height: 500px;
     overflow: hidden;
@@ -328,6 +469,19 @@ export default {
       margin: 0 auto;
       display: block;
       width: 500px; height: 500px;
+    }
+  }
+  .data-section {
+    margin-left: 20px;
+    padding-top: 30px;
+    text-align: left;
+    .data-item {
+      margin-bottom: 5px;
+      padding: 5px;
+      border: 2px solid transparent;
+      &.active {
+        border-color: #409eff;
+      }
     }
   }
 }
