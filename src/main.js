9049fdadcf88b5c18f99dddb8f25b649012fc0b8
wukun <wu007kun@yeah.net>
2021-01-28 16:22:34 +0800

    commit

--- src/main.js
+++ src/main.js
@@ -1,7 +1,6 @@
 import { createApp } from 'vue'
 import App from './App.vue'
 import router from './router'
-import store from './store'
 
 const vueApp = createApp(App)
-vueApp.use(store).use(router).mount('#app')
+vueApp.use(router).mount('#app')
